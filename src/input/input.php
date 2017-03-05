7c5b312a1c381957a7e597b83df9448e6b03ce0a
UnessR <benmouloud.youness@gmail.com>
2017-03-05 23:26:56 +0000

    First Commit

---
+++ src/input/input.php
@@ -0,0 +1,47 @@
+<?php 
+
+namespace input;
+
+	class  input{
+		
+		
+		
+		
+		
+		public static function InputGen(){
+			
+			
+			return'<input type="text"/>';
+			
+			
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
